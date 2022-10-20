e3f867c64d359a99bfcef28fcbf47444ec9c2306
Anderson Nascimento <anderson.nascimento@delage.com.br>
2022-10-19 22:46:55 -0300

    csharp classe

---
+++ MembrosEstaticos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
+
+
+            Console.WriteLine("Circunferência: " + circ.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("f2", CultureInfo.InvariantCulture));
+        }
+
+
+    }
+}
